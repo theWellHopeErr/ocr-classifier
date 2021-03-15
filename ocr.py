3c1e57a0dbe31cb85ed258245fb409dc3500bb7b
Balaraman <balaramanmuthupandi@gmail.com>
2021-03-15 12:57:23 +0530

    Change the data extraction type
    Replace image_to_string function with image_to_data for metadata

--- ocr.py
+++ ocr.py
@@ -1,44 +1,60 @@
 ##############################
-# Usage: 
+# Usage:
 # python ocr.py img.ext
 ##############################
-
-import cv2 
-import pytesseract 
 import sys
+import os
+import json
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+import cv2
+import pytesseract
+from pytesseract import Output
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSARACT_PATH")
+print("[INFO] loading image...")
 img = cv2.imread(sys.argv[1])
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+print("[INFO] processing image...")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+ret, thresh1 = cv2.threshold(
+    gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
+dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+contours, hierarchy = cv2.findContours(
+    dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-im2 = img.copy() 
+im2 = img.copy()
 
-file = open("recognized.txt", "w+") 
-file.write("") 
-file.close() 
+file = open("recognized.json", "w+")
+file.write("[")
+file.close()
 
-for cnt in contours: 
-	x, y, w, h = cv2.boundingRect(cnt) 
-	
-	rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2) 
-	
-	cropped = im2[y:y + h, x:x + w] 
-	
-	file = open("recognized.txt", "a") 
-	
-	text = pytesseract.image_to_string(cropped) 
-	
-	file.write(text) 
-	file.write("\n") 
-	print(text)
-	
-	file.close 
+for idx, cnt in enumerate(contours):
+    x, y, w, h = cv2.boundingRect(cnt)
+
+    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cropped = im2[y:y + h, x:x + w]
+
+    file = open("recognized.json", "a")
+
+    print("[INFO] reading text...")
+    text = pytesseract.image_to_data(cropped, output_type=Output.DICT)
+
+    print("[INFO] writing text...")
+    file.write(json.dumps(text))
+    if idx != len(contours) - 1:
+        file.write(",")
+    else:
+        file.write("]")
+    # print(text)
+
+    file.close()
+print("[Finished] Data written to the file .\\recognised.json")
